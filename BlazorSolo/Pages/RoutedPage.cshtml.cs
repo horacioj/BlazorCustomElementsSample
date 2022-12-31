c69a7245bf753d71401f6b0d0de521ea8def2523
horacioj <horacioj@evotiva.com>
2022-12-30 21:49:05 -0300

    demo

---
+++ BlazorSolo/Pages/RoutedPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorSolo.Pages
+{
+    public class RoutedPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
